0c71c927c0f93aad0dd3c9a477c976faa6c16e72
Domninic <liamdomnic@gmail.com>
2021-11-08 00:17:08 +0300

    all the newsapp files

---
+++ app/src/main/java/com/test/newsapp/DetailsActivity.java
@@ -0,0 +1,50 @@
+package com.test.newsapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetailsActivity extends AppCompatActivity {
+    WebView webView;
+    ProgressBar loader;
+    String url="";
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_details);
+
+        Intent intent=getIntent();
+        url=intent.getStringExtra("url");
+        loader=findViewById(R.id.loader);
+        webView=findViewById(R.id.webView);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.setWebViewClient(new WebViewClient(){
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                loader.setVisibility(View.VISIBLE);
+                view.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, final String url) {
+                loader.setVisibility(View.GONE);
+            }
+        });
+
+        webView.loadUrl(url);
+    }
+}
